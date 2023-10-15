e6fa4c8fc0e450066afc0a362de6479d738d44ac
karan <karan_virmani@yahoo.com>
2023-10-15 06:26:49 +0000

    mavenfinal

---
+++ konvicted.gang/src/main/java/konvictgroup/App.java
@@ -0,0 +1,13 @@
+package konvictgroup;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
